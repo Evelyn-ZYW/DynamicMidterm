244981468f88e20c040271d4aa610fcc53217842
= <=>
2020-03-16 12:59:00 -0700

    new files

---
+++ 2.js
@@ -0,0 +1,100 @@
+var end_quiz = [
+  {
+    text1: "You have completed non-verbal quizes!!!",
+  },
+  {
+    text1: "You have completed verbal quizes!!!",
+  }
+];
+var feed_back = [
+  {
+    text1: "You are correct!!!",
+    img1: "imgs/correct.png",
+  },
+  {
+    text1: "You are wrong!!!",
+    img1: "imgs/wrong.png",
+  }
+];
+
+var t1_ind = 0;
+var t1_qs = [
+  {
+    img1: "imgs/q1_1.png",
+    img2: "imgs/question.png",
+    text1: "=",
+    img3: "imgs/turtle.png",
+    Text2: "or",
+    img4: "imgs/bunny.png",
+    answer: 2,
+    correctexplain: "because it is correct"
+  }
+];
+
+
+var t2_ind = 0;
+var t2_qs = [
+  {
+    img1: "imgs/q2_1.png",
+    text1: "What does it look like from its left side?",
+    img2: "imgs/q2_2.png",
+    answer: 3,
+    correctexplain: "because it is correct"
+  },
+  {
+    img1: "imgs/q3_1.png",
+    text1: "How does it look like from looking above?",
+    img2: "imgs/q3_2.png",
+    answer: 2,
+  }
+];
+
+
+var t3_ind = 0;
+var t3_qs = [
+  {
+    img1: "imgs/match1.png",
+    text1: "How many moves you need to get to below result?",
+    img2: "imgs/match2.png",
+    answer: 1,
+    correctexplain: "because it is correct"
+  }
+];
+
+var t4_ind = 0;
+var t4_qs = [
+  {
+    text1: "Statement 1: All cats are mammals.",
+    text2: "Statement 2: All mammals are viviparity.",
+    img1: "imgs/q5_1.png",
+    text3: "All cats are viviparity.",
+    answer: 1,
+    correctexplain: "because it is correct"
+  },
+  {
+    text1: "Statement 1: Human are mammals.",
+    text2: "Statement 2: The city is full of mammals.",
+    img1: "imgs/q6_1.png",
+    text3: "The city is full of human.",
+    answer: 2
+  }
+];
+ 
+var t5_ind = 0;
+var t5_qs = [
+  {
+    text1: "If we allow same sex marriage, they would not give birth to babies.",
+    text2: "Human being will extinct.",
+    img1: "imgs/q7_1.png",
+    text3: "Do you agree with above conversation?",
+    answer: 2,
+    correctexplain: "because it is correct"
+  },
+  {
+    text1: "Birds have beaks.",
+    text2: "Turtles have beaks.",
+    img1: "imgs/q8_1.png",
+    text3: "Do you agree with below? <br><br> Therefore bird is turtle.",
+    answer: 2,
+  }
+];
